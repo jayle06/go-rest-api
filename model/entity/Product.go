a06bafa9d5b854f3ec1dd668dfc772a02a654eca
jayle06 <mhoang0611@gmail.com>
2021-06-21 10:32:48 +0700

    update image

--- model/entity/Product.go
+++ model/entity/Product.go
@@ -1,13 +1,13 @@
 package entity
 
 type Product struct {
-	Id         int64    `json:"id"`
-	CategoryId int64    `json:"category_id"`
-	Image      []string `json:"image"`
-	Name       string   `json:"name"`
-	Price      int64    `json:"price"`
-	IsSale     bool     `json:"is_sale"`
-	CreatedAt  int64    `json:"created_at"`
-	ModifiedAt int64    `json:"modified_at"`
-	Rating     float64  `json:"rating"`
+	Id         int64   `json:"id"`
+	CategoryId int64   `json:"category_id"`
+	Image      []Image `json:"image"`
+	Name       string  `json:"name"`
+	Price      int64   `json:"price"`
+	IsSale     bool    `json:"is_sale"`
+	CreatedAt  int64   `json:"created_at"`
+	ModifiedAt int64   `json:"modified_at"`
+	Rating     float64 `json:"rating"`
 }
